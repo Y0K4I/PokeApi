6ff27d8d8df5859e47359ddea055a729bd938d04
Y0K4I <skoralex02@gmail.com>
2020-10-19 16:48:10 +0300

    fix cors

--- index.js
+++ index.js
@@ -1,25 +1,5 @@
-const request = require('request');
-
 const app = require('./app')
 const port = process.env.PORT || 5000
 
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
-
-app.get('/PokeApi/getPokemons', (req, res) => {
-request(
-    {url: 'https://pokemonapishort.herokuapp.com/PokeApi/getPokemons' },
-    (error, response, body) => {
-    if (error || response.statusCode !== 200) {
-        return res.status(500).json({ type: 'error', message: err.message });
-    }
-  
-    res.json(JSON.parse(body));
-    }
-)
-});
 
 app.listen(port, () => console.log(`Server has been started on ${port}`))
