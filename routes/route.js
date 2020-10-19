687bec2231c74d19c0bcd94b308057fd3059d4df
Y0K4I <skoralex02@gmail.com>
2020-10-19 17:45:09 +0300

    fix cors

--- routes/route.js
+++ routes/route.js
@@ -4,6 +4,14 @@ const router = express.Router()
 const controller = require('../controllers/pokemon')
 const filter = require('../controllers/filter')
 
+router.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
+
 router.get('/getPokemonsStats', controller.getPokemonsStats) // database parse
 
 router.get('/getStatOf/:id', controller.getPokemonStats)
