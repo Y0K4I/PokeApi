d43eaf2de267ca6d478b8f4be5a7b4506e7cf4cd
Y0K4I <skoralex02@gmail.com>
2020-10-26 16:32:13 +0300

    FIX

--- routes/route.js
+++ routes/route.js
@@ -7,7 +7,7 @@ const filter = require('../controllers/filter')
 router.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    // res.append('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 
