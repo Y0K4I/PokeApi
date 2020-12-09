a8c6f9f860074aca51dbb04b94a2c722397b62b2
Alex <67697693+Y0K4I@users.noreply.github.com>
2020-12-09 14:49:32 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURL: 'mongodb+srv://Yokai:UniPass9637@cluster0.8zvgx.gcp.mongodb.net/NewDB?retryWrites=true&w=majority'
+    mongoURL: 'mongodb+srv://Yokai:T1rAfLYcsBKsAkkX@cluster0.8zvgx.gcp.mongodb.net/NewDB?retryWrites=true&w=majority'
 }
